a6d85c0a9c2d8e8476e9af41903b992416828d89
seungeon <tmddjs210@naver.com>
2020-01-01 15:16:27 +0900

    레트로핏 세팅중

---
+++ MapoGreen/app/src/main/java/com/mtjin/mapogreen/api/ApiClient.java
@@ -0,0 +1,11 @@
+package com.mtjin.mapogreen.api;
+
+import retrofit2.Retrofit;
+
+public class ApiClient {
+
+    private static final String BASE_URL = "http://ec2-52-79-235-166.ap-northeast-2.compute.amazonaws.com:3000/";
+    private static Retrofit retrofit;
+
+
+}
---
+++ MapoGreen/app/src/main/java/com/mtjin/mapogreen/api/ApiInterface.java
@@ -0,0 +1,4 @@
+package com.mtjin.mapogreen.api;
+
+public interface ApiInterface {
+}
