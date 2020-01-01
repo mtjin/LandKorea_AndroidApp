2d7b7d1cfcea724ccb22038ca34ed53d488bdef7
seungeon <tmddjs210@naver.com>
2020-01-01 16:43:45 +0900

    맵액티비티 레이아웃 및 프로토타입 정의 완료

--- MapoGreen/app/src/main/java/com/mtjin/mapogreen/activities/MapActivity.java
+++ MapoGreen/app/src/main/java/com/mtjin/mapogreen/activities/MapActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 import com.mtjin.mapogreen.R;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 import net.daum.mf.map.api.MapPOIItem;
@@ -20,19 +27,31 @@ import net.daum.mf.map.api.MapView;
 import java.util.ArrayList;
 
 
-public class MapActivity extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener, MapView.OpenAPIKeyAuthenticationResultListener {
+public class MapActivity extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener, MapView.OpenAPIKeyAuthenticationResultListener, View.OnClickListener {
     //xml
     MapView mMapView;
     ViewGroup mMapViewContainer;
     EditText mSerachEdit;
     Button mOkButton;
+    private Animation fab_open, fab_close;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab1, fab2;
+    RelativeLayout mLoaderLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        //binding
         mSerachEdit = findViewById(R.id.map_et_search);
         mOkButton = findViewById(R.id.map_btn_ok);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fab = findViewById(R.id.fab);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+        mLoaderLayout = findViewById(R.id.loaderLayout);
 
         //권한요청
         tedPermission();
@@ -41,10 +60,13 @@ public class MapActivity extends AppCompatActivity implements MapView.MapViewEve
         mMapViewContainer = findViewById(R.id.map_mv_mapcontainer);
         mMapViewContainer.addView(mMapView);
 
-        //리스너 등록
+        //리스너 세팅
         mMapView.setMapViewEventListener(this); // this에 MapView.MapViewEventListener 구현.
         mMapView.setPOIItemEventListener(this);
         mMapView.setOpenAPIKeyAuthenticationResultListener(this);
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
 
         mMapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
 
@@ -73,6 +95,45 @@ public class MapActivity extends AppCompatActivity implements MapView.MapViewEve
                 .setDeniedMessage(getResources().getString(R.string.permission_1))
                 .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
                 .check();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.fab:
+                anim();
+                FancyToast.makeText(this, "1번 버튼: 해당 좌표 중심으로 1km 검색" +
+                        "\n2번 버튼: 1km 검색한 결과 자세히보기", FancyToast.LENGTH_SHORT, FancyToast.INFO, true).show();
+                break;
+            case R.id.fab1:
+                mLoaderLayout.setVisibility(View.VISIBLE);
+                anim();
+                mLoaderLayout.setVisibility(View.GONE);
+                break;
+            case R.id.fab2:
+                mLoaderLayout.setVisibility(View.VISIBLE);
+                anim();
+                mLoaderLayout.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+    public void anim() {
+
+        if (isFabOpen) {
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+        }
     }
 
 
